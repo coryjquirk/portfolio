49ea95ab4925cb479a19053b459dfdfa9104d52f
Cory Quirk <coryjquirk@gmail.com>
2020-12-14 22:14:29 -0600

    favicon in footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useStoreContext } from "../../utils/GlobalState";
 import "./style.css";
+import Favicon from "./favicon.ico.png"
 
 function Footer() {
   const [store] = useStoreContext();
@@ -12,7 +13,8 @@ function Footer() {
             <a href="https://www.linkedin.com/in/cory-quirk/" class="fa fa-linkedin"></a>
             <a href="https://www.flickr.com/photos/152958688@N03/" class="fa fa-flickr"></a>
             <a href="https://www.github.com/coryjquirk" class="fa fa-github"></a>
-            .- .-.. -- --- ... -  - .... . .-. .©2020
+            <span id="cuteMorse">.- .-.. -- --- ... -  - .... . .-. .</span>©2020
+            <a href="https://coryjquirk.herokuapp.com/"><img id="footerFavicon" src={Favicon} alt="favicon"/></a>
         </p>
     </div>
     );
