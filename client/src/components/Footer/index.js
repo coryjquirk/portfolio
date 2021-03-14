2fdfc84210bed237d8ba43d0b69b7250b5747bce
Cory Quirk <coryjquirk@gmail.com>
2021-03-14 07:37:40 -0500

    responding to lighthouse - added meta, cleared console errors

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,24 +4,24 @@ import Favicon from "./favicon.png";
 
 function Footer() {
   return (
-    <div class="footer">
-        <a href="https://coryjquirk.herokuapp.com/">
-          <img id="footerFavicon" src={Favicon} alt="favicon" />
-        </a>
-        2021<span id="cuteMorse">.- .-.. -- --- ... - - .... . .-. . </span>
-        <a
-          href="https://www.instagram.com/coryquirk/"
-          class="fa fa-instagram"
-        ></a>
-        <a
-          href="https://www.linkedin.com/in/cory-quirk/"
-          class="fa fa-linkedin"
-        ></a>
-        <a
-          href="https://www.flickr.com/photos/152958688@N03/"
-          class="fa fa-flickr"
-        ></a>
-        <a href="https://www.github.com/coryjquirk" class="fa fa-github"></a>
+    <div className="footer">
+      <a href="https://coryjquirk.herokuapp.com/">
+        <img id="footerFavicon" src={Favicon} alt="favicon" />
+      </a>
+      2021<span id="cuteMorse">.- .-.. -- --- ... - - .... . .-. . </span>
+      <a
+        href="https://www.instagram.com/coryquirk/"
+        className="fa fa-instagram"
+      ></a>
+      <a
+        href="https://www.linkedin.com/in/cory-quirk/"
+        className="fa fa-linkedin"
+      ></a>
+      <a
+        href="https://www.flickr.com/photos/152958688@N03/"
+        className="fa fa-flickr"
+      ></a>
+      <a href="https://www.github.com/coryjquirk" className="fa fa-github"></a>
     </div>
   );
 }
