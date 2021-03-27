78da46c4b365c1ef542085425fbe9b4c8d7803c9
Cory Quirk <coryjquirk@gmail.com>
2021-03-26 22:14:54 -0700

    blog format update

--- client/src/components/Blog/Posts/Post4/index.js
+++ client/src/components/Blog/Posts/Post4/index.js
@@ -39,20 +39,27 @@ function Post3() {
             />
           </div>
           <p>
-            The <a href="https://www.raspberrypi.org/products/raspberry-pi-pico/" target="_blank">Raspberry Pi RP2040</a> (Pico) costs just $4 per board as a new 2021
-            addition to their line of products. From this microcontroller, you
-            can build circuits to sense outside stimuli or control other parts
-            in the device you're building. Microcontrollers play an important
-            hidden role in modern technology for their ability to store small
-            amounts of code for dedicated functions in consumer electronics.
-            They consume little power and give their device a low-cost "brain,"
-            but they process slowly and do not use memory as contrasted with a
-            full-fledged PC that contains a microprocessor (your AMD or Intel
-            CPU).
+            The{" "}
+            <a
+              href="https://www.raspberrypi.org/products/raspberry-pi-pico/"
+              target="_blank"
+            >
+              Raspberry Pi RP2040
+            </a>{" "}
+            (Pico) costs just $4 per board as a new 2021 addition to their line
+            of products. From this microcontroller, you can build circuits to
+            sense outside stimuli or control other parts in the device you're
+            building. Microcontrollers play an important hidden role in modern
+            technology for their ability to store small amounts of code for
+            dedicated functions in consumer electronics. They consume little
+            power and give their device a low-cost "brain," but they process
+            slowly and do not use memory as contrasted with a full-fledged PC
+            that contains a microprocessor (your AMD or Intel CPU).
           </p>
           <p>
-            This sounded like a great opportunity for my first computer soldering project. In order to get electricity flowing in and out of the
-            microcontroller from the breadboard it's sitting on, I needed to
+            This sounded like a great opportunity for my first computer
+            soldering project. In order to get electricity flowing in and out of
+            the microcontroller from the breadboard it's sitting on, I needed to
             securely connect GPIO (general purpose input/output) pins so
             electricity can flow in and out. The first few pins were not
             promising and I applied way too much solder. The board also came out
@@ -60,14 +67,20 @@ function Post3() {
           </p>
           <p>
             I live with a chronic autoimmune neuromuscular condition called
-            transverse myelitis that makes it difficult to keep steady hands
-            when using fine motor skills. I had to be extra careful to stabilize
-            both of my hands and it took a couple of small soldering iron burns
-            to find the right position. I'm also not the target demographic for
-            this product and someone with smaller hands would have an easier
-            time. When I told my dad about this project, he mentioned that his
-            mother spent some years performing soldering work at an electronics
-            company in Hibbing, Minnesota.
+            <a
+              href="https://www.mayoclinic.org/diseases-conditions/transverse-myelitis/symptoms-causes/syc-20354726#:~:text=Transverse%20myelitis%20is%20an%20inflammation,nerves%20send%20throughout%20the%20body."
+              target="_blank"
+            >
+              transverse myelitis
+            </a>{" "}
+            that makes it difficult to keep steady hands when using fine motor
+            skills. I had to be extra careful to stabilize both of my hands and
+            I got a couple of small soldering iron burns before I found the
+            right position. I'm also not the target demographic for this product
+            and someone with smaller hands would have an easier time. When I
+            told my dad about this project, he mentioned that his mother spent
+            some years performing soldering work at an electronics company in
+            Hibbing, Minnesota.
           </p>
           <ModalImage
             small={before}
@@ -87,8 +100,11 @@ function Post3() {
             Here's a demo of a simple "LED button press" circuit, with
             MicroPython code written with the Thonny Python IDE on Raspberry Pi.
             You can choose to program the Pico with either C or MicroPython.
-            MicroPython is a Python interpreter written in specifically for
-            microcontrollers like the Pico.{" "}
+            <a href="https://micropython.org/" target="_blank">
+              MicroPython
+            </a>{" "}
+            is a Python interpreter written in specifically for application in
+            microcontrollers.{" "}
           </p>
           <ModalImage
             small={button}
@@ -99,22 +115,25 @@ function Post3() {
           />
           <p>
             Part of me was expecting some kind of fiery explosion or lightning
-            strrike when I gave electricity to my first soldering job, but so
-            far so good. For $4 a board there's not a ton at risk. For a
-            production scale product, you would of course want to buy many
-            microcontrollers (one for each device you build), so in an
-            industrial context I'm sure there are much cheaper boards with just
-            the bare minimum amount of features.
+            strike when I gave electricity to my first soldering job, but so far
+            so good. For $4 a board there's not a ton at risk. For a production
+            scale product, you would of course want to buy many microcontrollers
+            (one for each device you build), so in an industrial context I'm
+            sure there are much cheaper boards with just the bare minimum amount
+            of features.
           </p>
           <p>
             The Pico seems like an ideal product for learners already familiar
             with the Raspberry Pi platform to get started with microcontrollers.
-            I'm working with CircuitPython, a derivative forked from
-            MicroPython, for on a Nordic Semiconductor nRF52840 board from
-            Sparkfun. I'll write about that more soon but so far I'm happy to
-            have programmed simple tasks on the SparkFun board, like basic LED
-            programming and initiating a basic bluetooth connection with my
-            smartphone.
+            I'm working with{" "}
+            <a href="https://circuitpython.org/" target="_blank">
+              CircuitPython
+            </a>
+            , a derivative forked from MicroPython, for a Nordic Semiconductor
+            nRF52840 board from Sparkfun. I'll write about that more soon, but
+            so far I'm happy to have programmed simple tasks on the SparkFun
+            board like basic LED controls and initiating a basic bluetooth
+            connection with my smartphone.
           </p>
           <ModalImage
             small={after}
--- client/src/components/Blog/index.js
+++ client/src/components/Blog/index.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import "./style.css";
 import { Container } from "../Grid";
 import self from "./self.jpg";
-import Post1 from "./Posts/Post1"
-import Post2 from "./Posts/Post2"
-import Post3 from "./Posts/Post3"
-import Post4 from "./Posts/Post4"
+import Post1 from "./Posts/Post1";
+import Post2 from "./Posts/Post2";
+import Post3 from "./Posts/Post3";
+import Post4 from "./Posts/Post4";
 
 function Blog() {
   const [opened, openApps] = useState(false);
@@ -31,71 +31,58 @@ function Blog() {
       </div>
       <hr className="headHr" />
       <div id="blog">
+        <div>
+          <div id="blogList">
+            <Post4></Post4>
+            <Post3></Post3>
+            <Post2></Post2>
+            <Post1></Post1>
+          </div>
 
-      <div>
-        <div id="blogList">
-          <Post4></Post4>
-          <Post3></Post3>
-          <Post2></Post2>
-          <Post1></Post1>
-         </div>
-
-        <div id="about">
-          <h3>about me</h3>
-          <p>
-            I'm a full stack web developer looking for full time remote work and
-            small project work. I built this site with React to share my
-            projects.{" "}
-          </p>
-          <p>
-            {" "}
-            If you're an employer, <a href="/contact">over here</a> you can find
-            my LinkedIn profile, resumé, and a catch-all cover letter. I stay
-            actively coding and you can visit my GitHub to see what I'm working
-            on.
-          </p>
-          <p id="break">-. --- .--</p>
-
-          <p>
-            &emsp;I earned a full stack web development certificate in Fall 2020
-            from the University in Minnesota, and a B.A. in anthropology in 2018
-            from Hamline University. I have professional experience using
-            Spanish and I've studied Norwegian since 2018.
-          </p>
-          <p>
-            &emsp;With Hamline University, a highlight of my undergrad was a
-            2017{" "}
-            <a
-              href="https://www.hamline.edu/hunewsdetail.aspx?id=4295030203"
-              target="_blank"
-            >
-              short course{" "}
-            </a>
-            in Siem Reap, Cambodia about the blooming tourist industry around
-            the Angkor temple complex. In my independent research, I studied the
-            phenomenon of noise pollution — the resulting sound works can be
-            found <a href="sound">over here</a>. Another formative experience
-            was working with other archaeologists on burial recovery sites,
-            where we restored disturbed pre-colonial grave sites
-            under the leadership of descendent communities.{" "}
-            <a
-              href="https://www.startribune.com/after-3-years-minnetonka-burial-mound-restoration-reaches-completion/456894483/"
-              target="_blank"
-            >
-              Here's
-            </a>{" "}
-            a Star Tribune article about one such site I worked on.
-          </p>
-          <p>
-            &emsp;Right now I'm excited to get started in tech. I'm open to work
-            and taking on new projects. Thank you for visiting!{" "}
-          </p>
-          <p id="blogDate">-March 2021</p>
+          <div id="about">
+            <h3>about me</h3>
+            <p>
+              I'm a full stack web developer looking for full time remote work
+              and small project work. I built this site with React to share my
+              projects.{" "}
+            </p>
+            <p>
+              {" "}
+              If you're an employer, <a href="/contact">over here</a> you can
+              find my LinkedIn profile, resumé, and a catch-all cover letter. I
+              stay actively coding and you can visit my GitHub to see what I'm
+              working on.
+            </p>
+            <div id="aboutmeIcons">
+              <a
+                href="https://www.instagram.com/coryquirk/"
+                className="fa fa-instagram"
+              ></a>
+              <a
+                href="https://www.linkedin.com/in/cory-quirk/"
+                className="fa fa-linkedin"
+              ></a>
+              <a
+                href="https://www.flickr.com/photos/152958688@N03/"
+                className="fa fa-flickr"
+              ></a>
+              <a
+                href="https://www.github.com/coryjquirk"
+                className="fa fa-github"
+              ></a>
+            </div>
+            <p>
+              &emsp;Right now I'm actively searching for a developer position
+              and open to taking on new projects. Thank you for visiting!{" "}
+            </p>
+            <p id="blogDate">-March 2021</p>
+          </div>
+          <div className="ghostDiv"></div>
         </div>
-        <div className="ghostDiv"></div>
-      </div>
-      <img id="self" src={self}></img>
-      <p className="mutedBlog" id="theseMine">all photos are mine unless otherwise stated or readily apparent.</p>
+        <img id="self" src={self}></img>
+        <p className="mutedBlog" id="theseMine">
+          all photos are mine unless otherwise stated or readily apparent.
+        </p>
       </div>
     </Container>
   );
